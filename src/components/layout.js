3d3ac77e9dfa3fa485d3309ddce311ae37216d8d
Axel de la Torre <axeldelat@gmail.com>
2021-01-02 15:29:43 -0500

    signup form upload

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { StaticQuery, graphql } from "gatsby";
-import Nav from "./nav";
+import Nav from "./tmpNav";
 import Seo from "./seo";
 
 const Layout = ({ children, seo }) => (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import Layout from "../components/layout";
-import ArticlesComponent from "../components/articles";
 
 const IndexPage = () => {
+  const [restaurantState, setRestaurantState] = useState( 'holi' );
   const data = useStaticQuery(query);
+
+  console.log(restaurantState)
 
   return (
     <Layout seo={data.strapiHomepage.seo}>
-      <div className="uk-section">
-        <div className="uk-container uk-container-large">
-          <h1>{data.strapiHomepage.hero.title}</h1>
-          <ArticlesComponent articles={data.allStrapiArticle.edges} />
-        </div>
+      <div id="hero-section"className="h-5/6 flex flex-col justify-center">
+        <h2 className="text-3xl text-center">Comenzar es<br />Rápido, Fácil, Libre de Comisiones<br />  y sobretodo GRATIS ❤️</h2>
+        <form action={`https://www.misistemadepedidos.com/admin/public/signup?restaurant_name=${restaurantState}&signup_source=markerante.com`} >
+          <div className=" overflow-hidden sm:rounded-md">
+            <div className="px-4 py-5 sm:p-6">
+              <div className="grid grid-cols-5 gap-2">
+                <div className="col-span-4">
+                  <input type="text" name="restaurant_name" id="restaurant_name" autocomplete="given-name" className="py-2 px-4 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  placeholder="Nombre de Restaurante"
+                  onChange={e => setRestaurantState(e.target.value)}
+                  />
+                </div>
+                <button type="submit" className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                  Enviar
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
       </div>
     </Layout>
   );
